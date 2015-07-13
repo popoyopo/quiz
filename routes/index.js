04fe2e0810f032703ee5f49b7459b0e95b8a09ad
popoyopo <francisco.canales@gmail.com>
2015-07-13 21:53:17 +0200

    Se añade la funcionalidad de crear preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,10 @@ router.get('/', function(req, res) {
 router.param('quizId',						    quizController.load);
 
 router.get('/quizes',							quizController.index);
+router.get('/quizes/new',						quizController.new);
 router.get('/quizes/:quizId',					quizController.show);
 router.get('/quizes/:quizId/answer',			quizController.answer);
 router.get('/author',							quizController.author);
+router.post('/quizes/create',						quizController.create);
 
 module.exports = router;
