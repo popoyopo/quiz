5c8e83c066fdca244a9ea576aaa4f1e98a53f79f
popoyopo <francisco.canales@gmail.com>
2015-07-13 22:41:13 +0200

    Se añade control de errores

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -14,29 +14,6 @@ exports.load = function (req,res,next,quizId){
 	).catch(function(error){next(error);});
 };
 
-
-// GET /quizes/:id
-exports.show = function(req,res){
-	models.Quiz.find(req.params.quizId).then(
-		function(quiz){
-			res.render('quizes/show',{quiz: req.quiz});
-	})
-};
-
-// GET /quizes/:id/answer
-exports.answer = function(req,res){
-	var resultado='Incorrecto';
-	if(req.query.respuesta===req.quiz.respuesta){
-		resultado='Correcto';
-	}
-	res.render('quizes/answer',{quiz:req.quiz,respuesta: resultado});
-};
-
-// GET /quizes/author
-exports.author = function(req,res){
-	res.render('author',{autor:'Francisco Canales'});
-};
-
 // GET /quizes
 exports.index = function(req,res){
 	var busqueda = req.query.search;
@@ -51,8 +28,25 @@ exports.index = function(req,res){
 	models.Quiz.findAll({where: ["pregunta like ?", busqueda],
 							order: [["pregunta","ASC"]]}).then(
 		function(quizes){
-			res.render('quizes/index',{quizes: quizes});
+			res.render('quizes/index',{quizes: quizes, errors:[]});
 	}).catch(function(error){next(error);});
+};
+
+// GET /quizes/:id
+exports.show = function(req,res){
+	models.Quiz.find(req.params.quizId).then(
+		function(quiz){
+			res.render('quizes/show',{quiz: req.quiz, errors:[]});
+	})
+};
+
+// GET /quizes/:id/answer
+exports.answer = function(req,res){
+	var resultado='Incorrecto';
+	if(req.query.respuesta===req.quiz.respuesta){
+		resultado='Correcto';
+	}
+	res.render('quizes/answer',{quiz:req.quiz,respuesta: resultado , errors:[]});
 };
 
 // GET /quizes/new
@@ -60,14 +54,26 @@ exports.new = function(req,res){
 	var quiz = models.Quiz.build( //crea objeto quiz
 			{pregunta: "Pregunta", respuesta: "Respuesta"}
 		);
-	res.render('quizes/new',{quiz:quiz});
+	res.render('quizes/new',{quiz:quiz , errors:[]});
 };
 
 // POST /quizes/create
 exports.create = function(req,res){ 
 	var quiz = models.Quiz.build( req.body.quiz );
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	})	//redirección HTTP (URL relativo) lista de preguntas
+
+	quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/new',{quiz: quiz, errors: err.errors});
+			} else {
+				quiz // guarda en DB los campos pregunta y respuesta de quiz
+				.save({fields: ["pregunta","respuesta"]})
+				.then( function(){res.redirect('/quizes')});
+			}	//redirección HTTP (URL relativo) lista de preguntas
+		});
+};
+
+// GET /quizes/author
+exports.author = function(req,res){
+	res.render('author',{autor:'Francisco Canales'});
 };
--- models/quiz.js
+++ models/quiz.js
@@ -1,8 +1,13 @@
 // Definición del modelo Quiz
 module.exports = function (sequelize,DataTypes){
 	return sequelize.define('Quiz',
-	{ 	pregunta : DataTypes.STRING,
-		respuesta :  DataTypes.STRING,
+	{ 	pregunta : {
+			type: DataTypes.STRING,
+			validate: {notEmpty:{msg:"-> Falta pregunta"}}
+		},
+		respuesta : {
+			type: DataTypes.STRING,
+			validate: {notEmpty:{msg:"-> Falta respuesta"}}}
 
 	});
 }
