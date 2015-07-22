8af4c86dbde463fbab65e214d0bda9772a2d0a6c
popoyopo <francisco.canales@gmail.com>
2015-07-22 22:14:48 +0200

    Sesión

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var session = require('express-session');
 var routes = require('./routes/index');
 
 var app = express();
@@ -20,10 +21,22 @@ app.use(partials());
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser('Quiz 2015'));
+app.use(session({secret: 'Quiz 2015', resave: false, saveUninitialized: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+    //guardar path en session.redir para después del login
+    if(!req.path.match(/\/login|\/logout/)){
+        req.session.redir= req.path;
+    }
+
+    //Hacer visible req.session en las vistas
+    res.locals.session = req.session;
+    next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -11,6 +12,12 @@ router.get('/', function(req, res) {
 //Autoload de comandos con :quizId
 router.param('quizId',						    	quizController.load);
 
+//definición de ruta de session
+router.get('/login',								sessionController.new);
+router.post('/login',								sessionController.create);
+router.get('/logout',								sessionController.destroy);
+
+//definición de las rutas quizes
 router.get('/quizes',								quizController.index);
 router.get('/quizes/new',							quizController.new);
 router.get('/quizes/:quizId(\\d+)',					quizController.show);
@@ -21,6 +28,7 @@ router.get('/quizes/:quizId(\\d+)/edit',			quizController.edit);
 router.put('/quizes/:quizId(\\d+)',					quizController.update);
 router.delete('/quizes/:quizId(\\d+)',				quizController.destroy);
 
+//definición de las rutas comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',		commentController.create);
 
