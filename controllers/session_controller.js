b0930f49d2ebbbb5c91bf6a50742c722d03bd3f6
popoyopo <francisco.canales@gmail.com>
2015-07-23 20:45:55 +0200

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+//MW de autorización de acceso http restringido
+exports.loginRequired = function(req,res,next){
+	if(req.session.user){
+		next();
+	} else {
+		res.redirect('/login');
+	}
+};
+
 // GET /login
 exports.new = function(req,res){ 
 	var errors = req.session.errors || {};
--- routes/index.js
+++ routes/index.js
@@ -19,14 +19,14 @@ router.get('/logout',								sessionController.destroy);
 
 //definición de las rutas quizes
 router.get('/quizes',								quizController.index);
-router.get('/quizes/new',							quizController.new);
+router.get('/quizes/new',							sessionController.loginRequired, quizController.new);
 router.get('/quizes/:quizId(\\d+)',					quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer',			quizController.answer);
 router.get('/author',								quizController.author);
-router.post('/quizes/create',						quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',			quizController.edit);
-router.put('/quizes/:quizId(\\d+)',					quizController.update);
-router.delete('/quizes/:quizId(\\d+)',				quizController.destroy);
+router.post('/quizes/create',						sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit',			sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)',					sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)',				sessionController.loginRequired, quizController.destroy);
 
 //definición de las rutas comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new',	commentController.new);
